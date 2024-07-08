a15941f82090293b2fc6f922df70ac49548df150
Adarsh <adarshchourasia6996@gmail.com>
2024-07-09 03:27:32 +0530

    checking

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const io = socketio(server);
 app.set('views', path.join(__dirname, 'views'));
 app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "public")));
-
+ 
 io.on("connection", function(socket){
     socket.on("send-location", function(data){
       io.emit("receive-location", {id: socket.id, ...data});
